08c51ee1529121e691a450d04156d2bac799a0fa
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2020-05-02 17:02:59 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,16 +7,23 @@ var server = app.listen(4000, () => { //Start the server, listening on port 4000
 
 var io = require('socket.io')(server); //Bind socket.io to our express server.
 
-app.use(express.static('public')); //Send index.html page on GET /
+app.use(express.static(__dirname + '/public')); //Send index.html page on GET /
+
+/*io.on('connection', (socket) => {
+    console.log("Someone connected."); //show a log as a new client connects.
+})*/
+
+io.on('connection', onConnection);
+
+var connectedSocket = null;
+function onConnection(socket){
+    connectedSocket = socket;
+}
 
 const SerialPort = require('serialport'); 
 const Readline = SerialPort.parsers.Readline;
 const port = new SerialPort('COM3'); //Connect serial port to port COM3. Because my Arduino Board is connected on port COM3. See yours on Arduino IDE -> Tools -> Port
-const parser = port.pipe(new Readline({delimiter: '\r\n'})); //Read the line only when new line comes.
-parser.on('data', (temp) => { //Read data
-    console.log(temp);
+const parser = port.pipe(new Readline()); //Read the line only when new line comes.
+parser.on('data', function (data) {
+    io.emit('data', { data: data });
 });
-
-io.on('connection', (socket) => {
-    console.log("Someone connected."); //show a log as a new client connects.
-})
